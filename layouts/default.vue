4d26190b2059b73a1d47dd4cb0fecc92f6b14ae7
Thya012 <samsathya012@gmail.com>
2025-01-17 08:26:18 +0700

    update new front

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="
+      dark:bg-gray-800
+      font-sans
+      bg-white
+      relative
+      h-screen
+      overflow-auto overflow-y-scroll
+    ">
+    <NavBar />
+    <nuxt />
+  </main>
+</template>
+<script>
+import NavBar from "@/components/navbar/nav-bar";
+
+export default {
+  components: { NavBar },
+};
+</script>
